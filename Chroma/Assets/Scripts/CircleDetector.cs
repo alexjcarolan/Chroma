662a8a9f9a95437671d37b6f6fc0eee2bf084849
AselKitulagoda <34165882+AselKitulagoda@users.noreply.github.com>
2020-02-24 18:13:37 +0000

    added basic circle detection

---
+++ Chroma/Assets/Scripts/CircleDetector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+
+public class CircleDetector : MonoBehaviour
+{
+    public static bool flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag == true)
+        {
+            GetComponent<CircleDetector>().enabled = false;
+        }
+        
+    }
+
+    public static void CallFunction()
+    {
+        
+        GameObject ob = GameObject.Find("drawing");
+        if (ob != null && flag == false)
+        {
+            int det = Dll.Load();
+            if (det == 1)
+            {
+                print("Detected");
+                flag = true; 
+               
+            }
+            
+        }
+    }
+
+    internal static class Dll
+    {
+        [DllImport("testdetect")]
+        internal static extern int Load();
+    }
+}
---
+++ Chroma/Assets/Scripts/DrawingsAttach.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawingsAttach : MonoBehaviour
+{
+    protected Transform stuckTo = null;
+    protected Vector3 offset = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<GraphicsLineRenderer>() != null)
+        {
+            HingeJoint hj = GetComponent<HingeJoint>();
+            hj.connectedBody = other.rigidbody;
+        }
+    }
+}
