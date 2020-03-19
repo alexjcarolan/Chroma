e547bce84bcf8b6c91041ef8ef851364298d88b1
AselKitulagoda <34165882+AselKitulagoda@users.noreply.github.com>
2020-03-19 15:42:29 +0000

    renamed level prefab

---
+++ Chroma/Assets/Editor/noAnimations.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+class CustomImporter : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        ModelImporter modelImporter = assetImporter as ModelImporter;
+        modelImporter.importAnimation = false;
+    }
+}
