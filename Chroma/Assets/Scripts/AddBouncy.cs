d4ea37a0336b3bec0c907b2c4440b7db1d70950f
AselKitulagoda <34165882+AselKitulagoda@users.noreply.github.com>
2020-03-20 18:28:27 +0000

    made level 1 changes

---
+++ Chroma/Assets/Scripts/AddBouncy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddBouncy : MonoBehaviour
+{
+    public PhysicMaterial mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionExit() {
+        if(this.gameObject.GetComponent<SphereCollider>().material.name != "Bounce")
+        {
+            gameObject.GetComponent<SphereCollider>().material = mat;
+        }
+    }
+}
--- Chroma/Assets/Scripts/UnlockTeleportArea.cs
+++ Chroma/Assets/Scripts/UnlockTeleportArea.cs
@@ -9,12 +9,14 @@ public class UnlockTeleportArea : MonoBehaviour
 {
     public GameObject bridge1;
     public GameObject bridge2;
-    public GameObject bridge3;
+    //public GameObject bridge3;
 
     public GameObject teleport1;
     public GameObject teleport2;
     public GameObject teleport3;
-    public GameObject teleport4;
+    public GameObject teleportBridge1;
+    public GameObject teleportBridge2;
+    //public GameObject teleport4;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +29,17 @@ public class UnlockTeleportArea : MonoBehaviour
     {   
         if(bridge1.GetComponent<Animator>().enabled == true)
         {
+            teleportBridge1.GetComponent<TeleportArea>().locked = false;
             teleport2.GetComponent<TeleportArea>().locked = false;
         }
         if (bridge2.GetComponent<Animator>().enabled == true)
         {
+            teleportBridge2.GetComponent<TeleportArea>().locked = false;
             teleport3.GetComponent<TeleportArea>().locked = false;
         }
-        if (bridge3.GetComponent<Animator>().enabled == true)
-        {
-            teleport4.GetComponent<TeleportArea>().locked = false;
-        }
+        //if (bridge3.GetComponent<Animator>().enabled == true)
+        //{
+        //    teleport4.GetComponent<TeleportArea>().locked = false;
+        //}
     }
 }
